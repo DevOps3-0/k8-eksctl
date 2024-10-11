a3d2e0da9dcf0a552705ade37c1888074b0ae4ae
RaghunagaY <yrnp2355@gmail.com>
2024-10-11 09:35:37 +0530

    argocd module configuration

--- modules/secret-manager/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "secret_arn" {
-  description = "ARN of the Secrets Manager secret"
-  value       = aws_secretsmanager_secret.argocd_endpoint.arn
-}
--- modules/secret-manager/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "secret_name" {
-  description = "Name of the secret in AWS Secrets Manager"
-  type        = string
-}
-
-variable "secret_value" {
-  description = "Value to store in the secret"
-  type        = string
-}
